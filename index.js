f586f947e332aa14df429bd96d05ba1066e16587
hasib1010 <asifmahmud664@gmail.com>
2024-01-28 15:37:25 +0600

    problem fixed

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors");
-require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
- 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bugptt7.mongodb.net/?retryWrites=true&w=majority`;
- 
+const uri = "mongodb+srv://coffeeMaster:MjYLdhgNbpnP2k1u@cluster0.bugptt7.mongodb.net/?retryWrites=true&w=majority";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -23,26 +21,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        console.log("Connected to MongoDB");
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // Continue with MongoDB operations here
+
+
+        const database = client.db("coffeeDB");
+        const coffeeCollection = database.collection("coffeeCollection");
+
+        app.post("/coffee", async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee); 
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result)
+        });
+        app.get('/coffee', async(req, res)=>{
+            const cursor = coffeeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+       
     }
 }
+
 run().catch(console.dir);
-
-
 
 app.get('/', (req, res) => {
     res.send("Coffee server is running");
 })
 
-
-
 app.listen(port, () => {
-    console.log(`coffe server is running on port ${port}`)
+    console.log(`Coffee server is running on port ${port}`);
 })
