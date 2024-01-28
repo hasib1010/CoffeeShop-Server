b33cf327f280cbd302af7ecc256478962242d908
hasib1010 <asifmahmud664@gmail.com>
2024-01-28 23:59:51 +0600

    coffee added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://coffeeMaster:MjYLdhgNbpnP2k1u@cluster0.bugptt7.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://coffeeMaster:GDizMM2jQAWuLWKE@cluster0.bugptt7.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,11 +33,11 @@ async function run() {
 
         app.post("/coffee", async (req, res) => {
             const newCoffee = req.body;
-            console.log(newCoffee); 
+            console.log(newCoffee);
             const result = await coffeeCollection.insertOne(newCoffee);
             res.send(result)
         });
-        app.get('/coffee', async(req, res)=>{
+        app.get('/coffee', async (req, res) => {
             const cursor = coffeeCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -46,7 +46,7 @@ async function run() {
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     } finally {
-       
+
     }
 }
 
